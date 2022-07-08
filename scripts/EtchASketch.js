8d58605a4035aeb1009fec6c9173d5b6a0983f0f
GimboidGit <alan.webster@hotmail.com>
2022-07-08 13:26:59 +0100

    Update index.html and EtchASketch.js
    index.html
    * Added a paragraph tag that is used to show the current grid size
    
    EtchASketch.js
    * New functions:
    ** SetGridSize
    ** Clear
    ** ResetCanvas
    ** UpdateGridSizeLabel
    * Comments above several function declarations to document purpose
    * Event handlers added for the two buttons on index.html
    * Updated "RenderCanvas" so that it now takes a param that specifies grid size

--- scripts/EtchASketch.js
+++ scripts/EtchASketch.js
@@ -1,8 +1,22 @@
-RenderCanvas();
+//Add event listeners
+const customSizeButton = document.getElementById("setGrid");
+customSizeButton.addEventListener("click", SetGridSize);
 
-function RenderCanvas()
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", ResetCanvas);
+
+
+let currentGridSize = 16;
+UpdateGridSizeLabel();
+
+RenderCanvas(currentGridSize);
+
+
+function RenderCanvas(numberOfBoxes)
 {
-    let gridSize = 16 * 16; //256
+    Clear();
+
+    let gridSize = numberOfBoxes * numberOfBoxes;
 
     for (let i = 1; i <= gridSize; i++)
     {
@@ -24,4 +38,50 @@ function RenderCanvas()
 function ColourIt(event)
 {
     event.target.style.backgroundColor = "black";
+}
+
+
+function SetGridSize(event)
+{
+    let size = 
+        Number(prompt("What grid size would you like? \r\n\r\n Specify a number between 1 and 100:"));
+
+    if (!Number.isInteger(size))
+    {
+        alert("That's not a valid number!");
+        return;
+    }
+
+    if (size < 1 && size > 100) 
+    {
+        alert("That's not a valid grid size!");
+        return;
+    }
+
+    currentGridSize = size;
+    
+    UpdateGridSizeLabel();
+    RenderCanvas(currentGridSize);
+}
+
+//This clears the Canvas, leaving it blank.
+function Clear()
+{
+    let container = document.querySelector(".grid-container");
+    //container.innerHTML = "";
+    container.textContent = "";
+}
+
+//Driven by button clickm this will clear the canvas, then render it again.
+function ResetCanvas()
+{
+    Clear();
+    RenderCanvas(currentGridSize);
+}
+
+//Updates the P element that displays the current grid size.
+function UpdateGridSizeLabel() 
+{
+    const p = document.querySelector(".current-size");
+    p.textContent = `Current Grid Size: ${currentGridSize}`
 }
